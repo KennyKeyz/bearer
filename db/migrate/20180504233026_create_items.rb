fcbe3ca458e6221325806008abefc1250e9d4928
Kenneth Woanyah <kenny.selorm@gmail.com>
2018-05-07 18:33:25 -0700

    status duplicate

--- db/migrate/20180504233026_create_items.rb
+++ db/migrate/20180504233026_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :status
+      t.string :status, :integer, default: 0
       t.string :tracking_number
       t.string :code
 
--- db/migrate/20180506182837_add_status_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToItem < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :items, :status, :integer, default: 0
-  end
-end
